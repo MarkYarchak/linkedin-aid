63e2aca430f7d698a13a6a88186c66b6725e05b4
Mark Yarchak <markyarchak.work@gmail.com>
2026-04-26 17:51:34 +0300

    feat: enhance scrapeLead to extract name, headling, and location info
    - add parsing of name, headline, and location from LinkedIn profile.
    - introduce helper functions to extract location and handle errors.
    - improve error handling with `couldNotScrapeLeadErr`.

--- src/content/scrape-lead.ts
+++ src/content/scrape-lead.ts
@@ -1,5 +1,40 @@
 export function scrapeLead() {
   console.log('Scrapping the lead');
 
-  return {};
+  const nameEl = document.querySelector('h1[data-anonymize="person-name"]')
+    || document.querySelector('main section div h1');
+
+  if (!nameEl) {
+    throw couldNotScrapeLeadErr();
+  }
+  const headerSection = nameEl.closest('section[class*="header"]');
+  if (!headerSection) {
+    throw couldNotScrapeLeadErr();
+  }
+
+  const name = nameEl.textContent.trim();
+  const headline = headerSection.querySelector('[data-anonymize="headline"]')?.textContent.trim();
+  const location = getLocation(headerSection);
+
+  const lead = {
+    name,
+    headline,
+    location,
+  };
+
+  console.log('Scrapped lead:', lead)
+
+  return lead;
+}
+
+function getLocation(headerSection: Element) {
+  return headerSection
+    .querySelector('svg path[d^="M8 1a5"]')
+    ?.closest('div')
+    ?.textContent
+    ?.trim();
+}
+
+function couldNotScrapeLeadErr() {
+  return new Error('Could not scrape the lead');
 }
