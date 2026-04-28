0bd7962db71a4d0ee02b1f32e91c3497f52f0599
Mark Yarchak <markyarchak.work@gmail.com>
2026-04-28 21:37:12 +0300

    feat: add `SalesApiProfiles2` type for extended profile data
    - introduce a new `SalesApiProfiles2` type to model comprehensive profile information.
    - include nested interfaces for structured handling of education, skills, volunteering experiences, and other related fields.

---
+++ src/types/salesApiProfiles2.ts
@@ -0,0 +1,94 @@
+export interface Artifact {
+  width: number;
+  fileIdentifyingUrlPathSegment: string;
+  height: number;
+}
+
+export interface DisplayImage {
+  artifacts: Artifact[];
+  rootUrl: string;
+}
+
+export interface SchoolResolutionResult {
+  entityUrn: string;
+  name: string;
+  schoolPictureDisplayImage: DisplayImage;
+  url: string;
+}
+
+export interface Education {
+  fieldsOfStudy: string[];
+  school: string;
+  degree: string;
+  schoolName: string;
+  eduId: number;
+  schoolResolutionResult: SchoolResolutionResult;
+}
+
+export interface LatestTouchPointActivity {
+  activityType: string;
+  performedAt: number;
+}
+
+export interface Skill {
+  name: string;
+  numOfEndorsement: number;
+}
+
+export interface VectorImage {
+  artifacts: Artifact[];
+  rootUrl: string;
+}
+
+export interface ProfileBackgroundPicture {
+  "com.linkedin.common.VectorImage": VectorImage;
+}
+
+export interface DateRange {
+  month?: number;
+  year: number;
+}
+
+export interface PictureInfo {
+  logo: string;
+}
+
+export interface CompanyResolutionResult {
+  entityUrn: string;
+  name: string;
+  companyPictureDisplayImage: DisplayImage;
+  pictureInfo?: PictureInfo;
+}
+
+export interface VolunteeringExperience {
+  role: string;
+  companyName: string;
+  cause?: string;
+  startedOn: DateRange;
+  companyResolutionResult?: CompanyResolutionResult;
+  company?: string;
+}
+
+export interface MemberBadges {
+  premium: boolean;
+  openLink: boolean;
+  jobSeeker: boolean;
+}
+
+export interface SalesApiProfiles2 {
+  educations: Education[];
+  latestTouchPointActivity: LatestTouchPointActivity;
+  skills: Skill[];
+  profileBackgroundPicture: ProfileBackgroundPicture;
+  entityUrn: string;
+  numOfConnections: number;
+  profilePictureDisplayImage: DisplayImage;
+  showTotalConnectionsPage: boolean;
+  blockThirdPartyDataSharing: boolean;
+  languages: any[];
+  noteCount: number;
+  volunteeringExperiences: VolunteeringExperience[];
+  relatedColleagueCompanyId: number;
+  numOfSharedConnections: number;
+  memberBadges: MemberBadges;
+}
