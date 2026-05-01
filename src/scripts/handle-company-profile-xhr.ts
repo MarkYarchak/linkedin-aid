718433ef7c89a965b9137d7dc92f241916540e23
Mark Yarchak <markyarchak.work@gmail.com>
2026-05-01 17:43:02 +0300

    feat: add company profile XHR handlers for data capture
    - implement handlers for capturing company data, extra data, and employee insights from the sales API.
    - extend `MessageType` constants to include new message types related to company data.

--- src/scripts/handle-company-profile-xhr.ts
+++ src/scripts/handle-company-profile-xhr.ts
@@ -1,1 +1,41 @@
-export function handleCompanyProfileXhr() {}
+import { handleXhr } from '@/scripts/xhr-interceptor';
+import { MessageType } from '@/constants/message-types';
+import { postWindowMessage } from '@/content/window-messages';
+import type { SalesApiCompanies } from '@/types/company/salesApiCompanies';
+import type { SalesApiCompanies2 } from '@/types/company/salesApiCompanies2';
+import type { SalesApiEmployeeInsights } from '@/types/company/salesApiEmployeeInsights';
+
+export function handleCompanyProfileXhr() {
+  handleXhr<SalesApiCompanies>(
+    /\/sales-api\/salesApiCompanies\/.*(?=.*companyPictureDisplayImage)(?=.*description)(?=.*industry)/,
+    (data, url) => {
+      postWindowMessage({
+        type: MessageType.COMPANY_CAPTURED,
+        url,
+        data,
+      });
+    },
+  );
+
+  handleXhr<SalesApiCompanies2>(
+    /\/sales-api\/salesApiCompanies\/.*decoration=(?!.*(description|industry)).*employeeCount/,
+    (data, url) => {
+      postWindowMessage({
+        type: MessageType.COMPANY_EXTRA_CAPTURED,
+        url,
+        data,
+      });
+    },
+  );
+
+  handleXhr<SalesApiEmployeeInsights>(
+    '/sales-api/salesApiEmployeeInsights',
+    (data, url) => {
+      postWindowMessage({
+        type: MessageType.COMPANY_INSIGHTS_CAPTURED,
+        url,
+        data,
+      });
+    },
+  );
+}
