a96cd0786b3d9e68c2a19306889017780c160c31
Mark Yarchak <markyarchak.work@gmail.com>
2026-05-20 23:26:47 +0300

    refactor: simplify CapturedDataView logic and app styles
    - remove unused `isSalesNavigatorCompanySearchUrl` with related logic.
    - streamline template structure by removing redundant `<div>` wrappers.
    - consolidate styles for `.captured-data-view` and remove `.tab-content` and `.leads-list`.
    - use CSS variables for app dimensions in popup styles.

--- src/components/CapturedDataView.vue
+++ src/components/CapturedDataView.vue
@@ -9,7 +9,6 @@ import {
   isSalesNavigatorLeadUrl,
   isSalesNavigatorCompanyUrl,
   isSalesNavigatorPeopleSearchUrl,
-  isSalesNavigatorCompanySearchUrl,
 } from '@/helpers/url-helpers';
 
 interface Props {
@@ -23,40 +22,33 @@ const { currentUrlCompany } = useCompanies(props.tabUrl);
 const isLeadPage = computed(() => isSalesNavigatorLeadUrl(props.tabUrl));
 const isCompanyPage = computed(() => isSalesNavigatorCompanyUrl(props.tabUrl));
 const isPeopleSearchPage = computed(() => isSalesNavigatorPeopleSearchUrl(props.tabUrl));
-const isCompanySearchPage = computed(() => isSalesNavigatorCompanySearchUrl(props.tabUrl));
 </script>
 
 <template>
   <div class="captured-data-view">
-    <div class="tab-content">
-      <template v-if="isLeadPage">
-        <div v-if="currentUrlLead" class="leads-list">
-          <LeadPreview :lead="currentUrlLead" />
-        </div>
-        <div v-else class="no-leads">
-          No leads collected yet. Navigate to a Sales Navigator lead profile to collect data.
-        </div>
-      </template>
+    <template v-if="isLeadPage">
+      <LeadPreview v-if="currentUrlLead" :lead="currentUrlLead" />
+      <div v-else class="no-leads">
+        No leads collected yet. Navigate to a Sales Navigator lead profile to collect data.
+      </div>
+    </template>
 
-      <template v-else-if="isPeopleSearchPage">
-        <LeadSearchPage :leads="leads" :tab-url="props.tabUrl" />
-      </template>
+    <template v-else-if="isPeopleSearchPage">
+      <LeadSearchPage :leads="leads" :tab-url="props.tabUrl" />
+    </template>
 
-      <template v-else-if="isCompanyPage || isCompanySearchPage">
-        <div v-if="currentUrlCompany" class="leads-list">
-          <CompanyPreview :company="currentUrlCompany" />
-        </div>
-        <div v-else class="no-leads">
-          No company collected yet. {{ isCompanySearchPage ? 'Collect companies from search results.' : 'Navigate to a Sales Navigator company profile to collect data.' }}
-        </div>
-      </template>
+    <template v-else-if="isCompanyPage">
+      <CompanyPreview v-if="currentUrlCompany" :company="currentUrlCompany" />
+      <div v-else class="no-leads">
+        No company collected yet. Navigate to a Sales Navigator company profile to collect data.
+      </div>
+    </template>
 
-      <template v-else>
-        <div class="no-leads">
-          Navigate to a Sales Navigator profile to collect data.
-        </div>
-      </template>
-    </div>
+    <template v-else>
+      <div class="no-leads">
+        Navigate to a Sales Navigator profile to collect data.
+      </div>
+    </template>
   </div>
 </template>
 
@@ -64,18 +56,7 @@ const isCompanySearchPage = computed(() => isSalesNavigatorCompanySearchUrl(prop
 .captured-data-view {
   display: flex;
   flex-direction: column;
-}
-
-.tab-content {
-  flex: 1;
-  overflow-y: auto;
   padding: 10px;
-}
-
-.leads-list {
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
 }
 
 .no-leads {
