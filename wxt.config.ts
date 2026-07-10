2f87bdb68673aeeb8513bbc1737fc4829cc06a4e
Mark Yarchak <markyarchak.work@gmail.com>
2026-07-10 16:29:39 +0300

    chore(config): append '(dev)' to app name in dev environment
    - add isDev logic to dynamically modify the app name in development mode.
    - ensure the app name now reflects the environment for easier identification.

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'wxt';
 
+const isDev = process.env.NODE_ENV === 'development';
+
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   srcDir: 'src',
   manifest: {
-    name: 'LinkedIn AID',
+    name: 'LinkedIn AID' + (isDev ? ' (dev)' : ''),
     description: 'LinkedIn SN data interceptor & assistant',
     permissions: ['scripting', 'storage', 'tabs'],
     host_permissions: ['*://*.linkedin.com/*'],
